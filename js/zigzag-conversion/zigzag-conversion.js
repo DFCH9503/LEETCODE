51dcac40d721aa9aaa897e028386e0338bb9afdc
DFCH9503 <dafechaparro@hotmail.com>
2025-04-03 10:23:44 -0500

    zigzag-conversion js

---
+++ js/zigzag-conversion/zigzag-conversion.js
@@ -0,0 +1,28 @@
+function zigzagConversion(s, numRows){
+    if (numRows == 1 || numRows >= s.length){
+        return s
+    }
+
+    let idx = 0, d = 1
+    let rows = new Array(numRows).fill().map( () => [])
+
+    for (const char of s) {
+        rows[idx].push(char)
+        if (idx === 0) {
+            d = 1
+        } else if (idx === numRows - 1) {
+            d = -1
+        }
+        idx += d
+    }
+
+    for (let i = 0; i < numRows; i++) {
+        rows[i] = rows[i].join('')
+    }
+
+    return rows.join('')
+
+}
+
+let s = "PAYPALISHIRING", numRows = 5
+console.log("answer to the first example is:",zigzagConversion(s, numRows))
