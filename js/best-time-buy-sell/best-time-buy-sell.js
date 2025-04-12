b0a3ceaefa1716294c253e53f8a5e3c7aaac53db
DFCH9503 <dafechaparro@hotmail.com>
2025-04-12 11:02:27 -0500

    best-time-buy-sell js

---
+++ js/best-time-buy-sell/best-time-buy-sell.js
@@ -0,0 +1,20 @@
+function maxProfit(prices){
+    let minPrice = prices[0], maxPrice = prices[0], res =0
+
+    for (price of prices){
+        if (price < minPrice){
+			minPrice = price
+			maxPrice = price
+		}else if (maxPrice < price){
+			maxPrice = price
+		}
+		if ((maxPrice - minPrice) > res){
+			res = maxPrice - minPrice
+		}
+	}
+	return res
+}
+
+
+let prices = [7,1,5,3,6,4]
+console.log(`The max profit for ${prices} is ${maxProfit(prices)}`)
